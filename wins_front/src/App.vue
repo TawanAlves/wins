7ae9644f32c09741eaa716adf7bb0637db2e1341
Tawan Alves <tas.tawan@gmail.com>
2024-12-18 00:26:44 -0300

    Feat: created footer

--- wins_front/src/App.vue
+++ wins_front/src/App.vue
@@ -23,6 +23,27 @@ import { RouterLink, RouterView } from 'vue-router'
       </div>
     </div>
   </header>
+  <footer>
+    <div class="footer_content">
+    <div class="termos">
+      <a href="#">Política de privacidade</a>
+      <a href="#">Termos de serviço</a>
+    </div>
+    
+    <img alt="logo footer" class="logo_wins" src="@/assets/logo_wins.png" width="194px" height="32px"/>
+
+    
+    <div class="footer_social">
+    <img alt="Footer Icon" class="footer_icon" src="@/assets/discord.svg" />
+    <img alt="Footer Icon" class="footer_icon" src="@/assets/instagram.svg" />
+    <img alt="Footer Icon" class="footer_icon" src="@/assets/youtube.svg" />
+    <img alt="Footer Icon" class="footer_icon" src="@/assets/tiktok.svg" />
+    <img alt="Footer Icon" class="footer_icon" src="@/assets/x.svg" />
+  </div>
+
+    <img alt="classificacao " class="classificacao" src="@/assets/classificacao.jpg"  height="32px" />
+    </div>
+  </footer>
 
   <RouterView />
 </template>
@@ -60,6 +81,30 @@ import { RouterLink, RouterView } from 'vue-router'
     display: flex;
     column-gap: 16px;
   }
+  footer{
+    display: flex;
+    background-color: #0F1B35;
+    padding: 180px 0 132px;
+    justify-content: center;
+    align-items: center;
+  }
+  .footer_content{
+    display: flex;
+    column-gap: 80px;
+    max-width: 1280px;
+  }
+
+  .footer_social{
+    display: flex;
+    column-gap: 5px;
+  }
+  .termos{
+    display: flex;
+    align-items: center;
+    column-gap: 30px;
+  }
+
+
  @media (min-width: 481px) and (max-width: 1014px){
   nav{
     display: none;
@@ -80,6 +125,11 @@ import { RouterLink, RouterView } from 'vue-router'
   .nav_right{
     column-gap: 10px;
   }
+  .footer_content{
+    flex-direction: column;
+    row-gap: 30px;
+    align-items: center;
+  }
  }
 
  @media (max-width: 481px){
@@ -91,6 +141,11 @@ import { RouterLink, RouterView } from 'vue-router'
     width: 30px;
   }
   
+  .footer_content{
+    flex-direction: column;
+    row-gap: 30px;
+    align-items: center;
+  }
 
  }
 

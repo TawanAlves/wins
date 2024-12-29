a73a3e4e8a3255ea86a4f47d13d7445363301200
Tawan Alves <tas.tawan@gmail.com>
2024-12-29 20:50:58 -0300

    Feat: update Peds

--- wins_front/src/views/PedsSection.vue
+++ wins_front/src/views/PedsSection.vue
@@ -8,9 +8,11 @@
       </div>
       <div class="images">
          <img alt="peds" class="off" src="@/assets/lojike.png" >
-         <img alt="peds" class="on" src="@/assets/tuta.png" >
+         <img alt="peds" class="off" src="https://wins.company/assets/TesteFrontEnd/peds/PEDCOM0001.webp" >
          <img alt="peds" class="off" src="@/assets/penunbro.png" >
       </div>
+      <p>{{pedsNames  }}</p>
+      <p>{{pedsDetails  }}</p>
 
 
       <div class="infos">
@@ -18,13 +20,68 @@
          <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
          <img alt="button" class="blue_btn" src="@/assets/btn_blue.png" width="232px" height="48px">
       </div>
-   </div>
+      </div>
    </section>
 </template>
 
 <script>
+   import api from "@/api/api.js";
    export default {
-      
+   components: {
+   },
+      data(){
+         return{
+            peds: [],    
+            pedsNames: [],   
+            pedsDetails: [],      
+            images: [
+        // URLs das suas imagens
+        'https://wins.company/assets/TesteFrontEnd/peds/PEDCOM0001.webp',
+        'https://wins.company/assets/TesteFrontEnd/peds/PEDCOM0001.webp',
+        'https://wins.company/assets/TesteFrontEnd/peds/PEDCOM0001.webp',
+      ]
+  
+         };
+      },
+      mounted() {
+         console.log("iniciou");
+         this.getAllPeds();
+      },
+      methods: {
+      // async getAllPeds() {
+      //     api.getAllPeds()
+      //    .then(response => console.log(response.data))
+      //    .catch(error => console.error('Erro:', error));
+      // }
+
+      async getAllPeds() {
+
+      // api.getAllUniforms()
+      // .then(response => console.log(response.data))
+      // .catch(error => console.error('Erro:', error));
+         try {
+         const getAllPeds = await api.getAllPeds();
+         this.peds = getAllPeds.data;
+         
+         this.pedsNames = this.peds.map(peds => peds.images[0]);
+
+         for (let i = 0; i < this.pedsNames.length; i++) {
+            const name = this.pedsNames[i];
+
+            const response = await api.getPed(name);
+
+            const imageUrl = response.data;
+            
+            this.pedsDetails.push({ name, imageUrl });
+            
+         }         
+         } catch (error) {
+         console.log(error);
+         }
+      },
+
+
+      }
    }
 </script>
 
@@ -33,10 +90,9 @@
       background-color: #070d1d;
       backdrop-filter: blur(119px);
       color: #ffffff;
-
-      display: flex;
-      justify-content: center;
       padding: 112px 0 170px;
+      display: flex;
+      justify-content: center; 
 
    }
    .header{
